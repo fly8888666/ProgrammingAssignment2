2710ece2b4bbbb7ae3d5bb2bf9cc493a1bf32f93
fly8888666 <ifei1992@hotmail.com>
2018-02-16 22:13:41 +0800

    Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+##Input: a matrix
+##A list contining functions to get, set the matrix, and get, set the inversed matrix
+##The function caches the inverse of the matrix
+  inversed_matrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inversed_matrix <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(a_matrix) inversed_matrix <<- a_matrix
+  getinverse <- function() inversed_matrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +25,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+##The input is
+  inversed_matrix <- x$getinverse()
+  if(!is.null(inversed_matrix)) {
+    message("getting cached data")
+    return(inversed_matrix)
+  }
+  data <- x$get()
+  inversed_matrix <- solve(data, ...)
+  x$setinverse(inversed_matrix)
+  inversed_matrix
+  
 }
+
+##Test
+##The following lines of codes test the above functions
+##Just uncomment and run on your computer
+#m <- matrix(c(1, 2, 3, 1, 1, 1, 1, 1, 5, 11), nrow=3, ncol=3)
+#new_m <- makeCacheMatrix(m)
+#cacheSolve(new_m)
+#cacheSolve(new_m)
